64128a04f6b649287c37e667565e2bbe8e68ee7d
Martin Wong <kcwong395@gmail.com>
2021-10-02 11:36:15 -0400

    Add a factory class to generate concrete scraper

---
+++ scraper/src/scraper/scraper_factory.py
@@ -0,0 +1,20 @@
+import logging
+
+from src.scraper.scraper import Scraper
+from src.scraper.scraper_sg import ScraperSG
+
+
+class ScraperFactory:
+
+    site_scrapers = {
+        "SG": ScraperSG,
+    }
+
+    """
+    input the site to scrap and return the corresponding site scraper
+    """
+    @staticmethod
+    def create(site: str) -> Scraper:
+        if site in ScraperFactory.site_scrapers:
+            return ScraperFactory.site_scrapers[site](target="Société Générale")
+        logging.error('Unrecognized Site')
