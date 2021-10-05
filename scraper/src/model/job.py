7b8d48466cb24668c46e423df974f5176f2bb8cb
Martin Wong <kcwong395@gmail.com>
2021-10-05 10:01:04 -0400

    add a constructor to fill all the fields

--- scraper/src/model/job.py
+++ scraper/src/model/job.py
@@ -1,6 +1,6 @@
 class Job:
-    '''
-    def __init__(self, ref: str, job_type: str, title: str, department: str, location: str, start_date: str, publish_date: str, link: str):
+    def __init__(self, ref: str, job_type: str, title: str, department: str, location: str, start_date: str,
+                 publish_date: str, link: str):
         self.ref = ref
         self.job_type = job_type
         self.title = title
@@ -9,9 +9,5 @@ class Job:
         self.start_date = start_date
         self.publish_date = publish_date
         self.link = link
-    '''
-
-    def __init__(self):
-        pass
 
 
