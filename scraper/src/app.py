c60c336e954ce36c75c1eb9414c04184b0340a00
Martin Wong <kcwong395@gmail.com>
2021-10-16 09:52:19 -0400

    load the config from yaml file and ready to handle multiple targets

--- scraper/src/app.py
+++ scraper/src/app.py
@@ -1,13 +1,28 @@
 from datetime import datetime
 import logging
+import yaml
 
-from src.scraper.scraper_factory import ScraperFactory
+from scraper.src.utils.db_utils import DbUtils
+from scraper.src.scraper.scraper_factory import ScraperFactory
 
 if __name__ == '__main__':
     logging.basicConfig(format="[%(asctime)s] [%(levelname)8s] --- %(message)s",
                         datefmt="%Y-%m-%d %H:%M:%S",
-                        filename='../log/' + datetime.strftime(datetime.today(), "%Y-%m-%d %H:%M:%S") + '.log',
+                        filename='../log/' + datetime.strftime(datetime.today(), "%Y-%m-%d %H-%M-%S") + '.log',
                         level=logging.INFO)
-    scraper = ScraperFactory.create("SG")
-    if scraper:
-        scraper.start()
+
+    with open("../../config.yaml", 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    DbUtils.connect(db_name=config['scraper']['db']['database'],
+                    username=config['scraper']['db']['username'],
+                    password=config['scraper']['db']['password'],
+                    host=config['scraper']['db']['host'],
+                    port=config['scraper']['db']['port'])
+    targets = config['scraper']['targets']
+    for target in targets:
+        scraper = ScraperFactory.create(target)
+        if scraper:
+            scraper.start()
+    DbUtils.close()
+
